7d52b67bd806918a445674b6c51c391c1f0e2ce4
thanglv2 <thanglv2@vng.com.vn>
2018-05-23 17:53:23 +0700

    NeuralLM

---
+++ nlm.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+
+class NeuralLM(object):
+	"""docstring for NeuralLM"""
+	def __init__(self, arg):
+		super(NeuralLM, self).__init__()
+		self.arg = arg
+
+		#Placeholders for input
+		self.wordIndices = tf.placeholder(tf.int32, [None, maxContentLength], name="inputWordIndices")
+		self.contexLength = tf.placeholder(tf.int32, [None], name='inputContextLengths')
+		self.wordTargets = tf.placeholder(tf.int32, [None], name='targetWordIndices')
+		self.newLr = tf.placeholder(tf.float32, [], name="newLearningRate")
+
+		#Embedding Layer
+		with tf.variable_scope("wordEmbeddingLayer"):
+			with tf.device("/cpu:0"):
+				self.embedding = tf.get_variable("wordEmbeddingMatrix", [vocabSize, embDim], dtype=tf.float32)
+				wordEmbedded = tf.nn.embedding_lookup(self.embedding, self.wordIndices, name="wordEmbeddedVectors")
+
+		#TODO: Dropout
+		#...
+
+		#RNN Graph
+		with tf.variable_scope("rnnWordLayer") as rnnVS:
+			rnnCells = []
+			for i in range(rnnLayers):
+				rnnCells.append(tf.contrib.rnn.LSTMBlockCell(rnnSize, name="rnnLayer-{}".format(i+1)))
+			rnnMultiCells = tf.contrib.rnn.MultiRNNCell(rnnCells)
+			contextEmbedded = tf.nn.dynamic_rnn(cell=rnnMultiCells, inputs=wordEmbedded, sequence_length=self.contexLength, dtype=tf.float32)
+			self.rnnVariables = tf.get_collection(key=tf.GraphKeys.TRAINABLE_VARIABLES, scope=rnnVS)
+
+			batchRange = tf.range(tf.shape(contextEmbedded)[0])  # <- batchSize: [0, 1, ..., batchSize - 1]
+			indices = tf.stack([batchRange, self.contexLength - 1], axis=1) # indices: [[0, L[0] - 1], [1, L[1] - 1], ..., [batchSize - 1, L[batchSize-1] -1]]
+			contextEncoded = tf.gather_nd(contextEmbedded, indices)
+
+		#Feed-Forward Layer
+		with tf.get_variable_scope("ffTargetLayer") as ffVS:
+			W = tf.get_variable("ffTargetWeight", shape=[rnnSize, nbClasses], dtype=tf.float32)
+			b = tf.get_variable("ffTargetBias", shape=[nbClasses], dtype=tf.float32)
+
+			logits = tf.nn.xw_plus_b(contextEncoded, self.W, self.b, name="feedforwardTargerLayer")
+
+			self.topKPred = tf.nn.top_k(logits, k=3, name="topKPredictions")  #TODO: set K outside
+			self.predictions = tf.argmax(logits, 1, name="predictions")
+			self.ffVariables = tf.get_collection(key=tf.GraphKeys.TRAINABLE_VARIABLES, scope=ffVS)
+
+		#Loss
+		with tf.get_variable_scope("loss"):
+			losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=self.wordTargets, name='losses')
+			self.cost = tf.reduce_mean(losses, name="cost")  # Use mean to make the gradient magnitude independent with batch size
+
+		self.globalStep = tf.Variable(0, name='globalStep', trainable=False)
+		self.lr = tf.Variable(0.0, trainable=False)
+
+		#Get weights of computation graph
+		tvars = tf.trainable_variables()
+
+		#Optimizer
+		self.optimizer = tf.train.GradientDescentOptimizer(self.lr)
+
+		#Calculating gradients and clipping by global norm if gradient over maxGradNorm
+		grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), maxGradNorm)
+
+		#Train
+		self.trainOp = self.optimizer.apply_gradients(zip(grads, tvars), global_step=self.globalStep)
+
+		#Learning rate update
+		self.updateLr = tf.assign(self.lr, self.newLr)
+
+	def assignLearningRate(self, sess, lrValue):
+		sess.run(sefl.updateLr, feed_dict={self.newLr: lrValue})
+
+	
+
+
+
+
+
+
+		
---
+++ test-tf.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+
+rvA = np.random.randn(1, 5, 4)  # Padding
+rvB = np.random.randn(1, 5, 4)
+
+print rvA.shape
+print rvA
+print rvB.shape
+print rvB
+
+
+rvC = np.concatenate((rvA, rvB), axis=0)
+print rvC.shape
+print rvC
+
+rvL = np.array([3, 5])
+print rvL.shape
+print rvL
+
+##Build TF Graph
+# Static RNN
+# X = tf.placeholder(tf.float32, shape=[None, 5, 4], name='input')
+# L = tf.placeholder(tf.int32, shape=[None], name='length')
+# rnnInput = tf.unstack(X, axis=1)
+# rnnCell = tf.nn.rnn_cell.BasicRNNCell(num_units=3)
+
+# rnnOut, rnnState = tf.nn.static_rnn(rnnCell, rnnInput, sequence_length=L, dtype=tf.float32)
+
+# Dynamic RNN
+X = tf.placeholder(tf.float32, shape=[None, None, 4], name='input')
+rnnInput = X
+L = tf.placeholder(tf.int32, shape=[None], name='length')
+rnnCell = tf.nn.rnn_cell.BasicRNNCell(num_units=3)
+rnnOut, rnnState = tf.nn.dynamic_rnn(rnnCell, rnnInput, sequence_length=L, dtype=tf.float32)
+
+# Get last output use tf.gather_nd
+batchRange = tf.range(tf.shape(rnnOut)[0])  # <- batchSize: [0, 1, ..., batchSize - 1]
+indices = tf.stack([batchRange, L - 1], axis=1) # indices: [[0, L[0] - 1], [1, L[1] - 1], ..., [batchSize - 1, L[batchSize-1] -1]]
+rnnLastOut = tf.gather_nd(rnnOut, indices)
+
+
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	# a = sess.run(rnnInput, feed_dict={X: rvC})
+	a, b, s, c = sess.run([rnnInput, rnnOut, rnnState, rnnLastOut], feed_dict={X: rvC, L: rvL})
+
+	# print a.shape
+	for item in a:
+		print "--------------------"
+		print item.shape
+		print item
+	# print b.shape
+	for item in b:
+		print "++++++++++++++++++++"
+		print item.shape
+		print item
+
+	for item in s:
+		print "===================="
+		print item.shape
+		print item
+
+	for item in c:
+		print "********************"
+		print item.shape
+		print item
